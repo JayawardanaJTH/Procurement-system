19a7d7192fd6f9625fd703c80d0c444394908257
Jayawardana JTH <tiran2323@gmail.com>
2020-10-15 17:56:16 +0530

    Configure settings

--- ProcumentSystem/src/com/procument/utills/Constant.java
+++ ProcumentSystem/src/com/procument/utills/Constant.java
@@ -1,6 +1,23 @@
 package com.procument.utills;
 
+/**
+ * 
+ * @author Tiran Harsha
+ *
+ */
 public class Constant {
 
+	//constants for query process
 	public static final String PROPERTY_FILE = "Properties.properties";
+	public static final String QUERY_ID = "id";
+	public static final String QUERY_TAG = "query";
+	
+	//constant for database configurations
+	public static final String DRIVER_NAME = "driver";
+	public static final String URL = "url";
+	public static final String PASSWORD = "password";
+	public static final String USERNAME = "username";
+	
+	//constant for query id s
+	public static final String GET_NAME = "name";
 }
